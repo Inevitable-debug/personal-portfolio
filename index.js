36988fd1265aec40c27f40da678aa1952a265edb
Inevitable-debug <62365519+Inevitable-debug@users.noreply.github.com>
2023-05-24 10:00:04 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var tablinks = document.getElementsByClassName("tab-links");
+const tablinksCount = tablinks.length;
 var tabcontents = document.getElementsByClassName("tab-contents");
-var sidemenu = document.getElementById("side-menu")
+const tabcontentsCount = tabcontents.length;
+const sidemenu = document.getElementById("side-menu");
 
 function opentab(tabname) {
-    for(tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-
-    for(tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
+    for (let i = 0; i < tablinksCount; i++) {
+        tablinks[i].classList.remove("active-link");
     }
     event.currentTarget.classList.add("active-link");
+
+    for (let i = 0; i < tabcontentsCount; i++) {
+        tabcontents[i].classList.remove("active-tab");
+    }
     document.getElementById(tabname).classList.add("active-tab");
 }
 
@@ -21,3 +23,32 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px"
 }
+
+/*function opentab(tabname) {
+    for (let i = 0; i < tablinksCount; i++) {
+        console.log(tablinks[i])
+    }
+    console.log(tablink.classList)
+
+    for (let i = 0; i < tabcontentsCount; i++) {
+        console.log(tabcontents[i])
+    }
+}
+*/
+/*var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+var sidemenu = document.getElementById("side-menu")
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    event.currentTarget.classList.add("active-link");
+
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    document.getElementById(tabname).classList.add("active-tab");
+}
+}
+*/
